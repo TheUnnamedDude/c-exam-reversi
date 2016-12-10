7fb7330ff99d355ad6a21d599b9bfa5bdb194a6e
Kevin Sillerud <kevin.sillerud@gmail.com>
2016-12-10 23:33:51 +0100

    Game should be done, needs logging

--- gamelogic.c
+++ gamelogic.c
@@ -3,32 +3,40 @@
 
 #define NUM_DIRECTIONS 8
 
+const Point DIRECTIONS[NUM_DIRECTIONS] = { {1, 0}, {1, 1}, {0, 1}, {-1, 1}, {-1, 0}, {-1, -1}, {0, -1}, {1, -1} };
+
 int check_add(Board *board, int x_pos, int y_pos, Tile tile, Point points[MAX_TILE_CHANGE]) {
     if (board->tiles[x_pos][y_pos] != NONE) {
         return 0;
     }
+
     int n_points = 0;
-    const Point directions[NUM_DIRECTIONS] = { {1, 0}, {1, 1}, {0, 1}, {-1, 1}, {-1, 0}, {-1, -1}, {0, -1}, {1, -1} };
+
     for (int i = 0; i < NUM_DIRECTIONS; i++) {
         int tiles_found = n_points;
         int x = x_pos;
         int y = y_pos;
-        while (x >= 0 && x < BOARD_WIDTH && y >= 0 && y < BOARD_HEIGHT)  {
+
+        while ((x+=DIRECTIONS[i].x) >= 0 && x < BOARD_WIDTH 
+            && (y+=DIRECTIONS[i].y) >= 0 && y < BOARD_HEIGHT)  {
+
             if (tiles_found >= MAX_TILE_CHANGE + 1) {
                 return n_points;
             }
-            x += directions[i].x;
-            y += directions[i].y;
+
             if (board->tiles[x][y] == tile) {
                 n_points = tiles_found;
             } else if (board->tiles[x][y] != tile && board->tiles[x][y] != NONE) {
+                tiles_found ++;
                 if (points == NULL)
                     continue;
-                Point *point = &points[tiles_found++];
+
+                Point *point = &points[tiles_found-1];
                 point->x = x;
                 point->y = y;
                 continue;
             }// Found NONE or the tile, we should stop looking
+
             break;
         }
     }
@@ -39,10 +47,11 @@ int check_add(Board *board, int x_pos, int y_pos, Tile tile, Point points[MAX_TI
 bool can_move(Board *board, Tile tile) {
     for (int x = 0; x < BOARD_WIDTH; x++) {
         for (int y = 0; y < BOARD_HEIGHT; y++) {
-            if (check_add(board, x, y, tile, NULL)) {
+            if (check_add(board, x, y, tile, NULL) > 0) {
                 return true;
             }
         }
     }
+
     return false;
 }
--- main.c
+++ main.c
@@ -13,19 +13,26 @@ int main(void) {
 
     int xPos;
     int yPos;
+
     Tile current_tile = WHITE;
+
     int nPoints = 0;
-    int player1_score = 2;
-    int player2_score = 2;
+
     update_scoreboard(&board, current_tile);
 
-    while (wait_for_input(&board, &xPos, &yPos)) {
-        if (can_move(&board, current_tile)) {
+    while ((can_move(&board, WHITE) || can_move(&board, BLACK)) 
+        && board.tiles_left > 0 && wait_for_input(&board, &xPos, &yPos)) { 
+        // The tripple can_move call is a little bit inefficient, but it should
+        // be so little overhead that its not worth messing around with
+        if (!can_move(&board, current_tile)) {
             current_tile = current_tile == WHITE ? BLACK : WHITE;
+            update_scoreboard(&board, current_tile);
             continue;
         }
+
         Point points[MAX_TILE_CHANGE];
         nPoints = check_add(&board, xPos, yPos, current_tile, points);
+
         if (nPoints > 0) {
             update_tile(&board, xPos, yPos, current_tile);
             for (int i = 0; i < nPoints; i++) {
@@ -33,16 +40,12 @@ int main(void) {
             }
             current_tile = current_tile == WHITE ? BLACK : WHITE;
         }
+
         update_scoreboard(&board, current_tile);
-        if (board.tiles_left <= 0) {
-            break;
-            // TODO: Print game over dialog
-        }
     }
+
     close_scoreboard(&board);
     show_game_over_screen(&board);
-
-    printf("User1: %s, User2: %s\n", board.player1_name, board.player2_name);
     close_board(&board);
 
     return 0;
